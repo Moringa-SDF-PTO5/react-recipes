1abf02f60b4b4c0f5ca4da4dd612946287de27a6
otsembo <okumu.otsembo@gmail.com>
2024-03-05 20:27:01 +0300

    add effects

--- src/components/home/SideNav.jsx
+++ src/components/home/SideNav.jsx
@@ -1,64 +1,32 @@
 import './SideNav.css'
 
-// nav menu items
-const menuItems = [
-    { id: 1, title: "Breakfast", count: 4 },
-    { id: 2, title: "Dessert", count: 9 },
-    { id: 3, title: "Meat and poultry", count: 7 },
-    { id: 4, title: "Pasta and pizza", count: 20 },
-    { id: 5, title: "Salads", count: 3 },
-    { id: 6, title: "Snacks", count: 12 },
-    { id: 7, title: "Soups", count: 8 },
-    { id: 8, title: "Breakfast", count: 4 },
-];
 
+function SideNav({onChangeMenu, categories}){
 
-function SideNav(){
-
-    const convertMenuToJSX = ({title, count, id}, index) => {
-        const key = crypto.randomUUID()
+    const convertMenuToJSX = ({title, count, id}) => {
         return (
-        <li key={key}>
-         <NavItem title={title} count={count} /> 
+        <li key={id}>
+         <NavItem title={title} count={count} onClick={() => onChangeMenu(title)} /> 
         </li>
         )
     }
 
-    const navItems = menuItems.map(convertMenuToJSX)
+    const navItems = categories.map(convertMenuToJSX)
 
     return (
         <nav>
             <ul className='nav-menu'>
                 { navItems }
-                {/* <li>
-                    <NavItem title="Breakfast" count="4"  />
-                </li>
-                <li>
-                    <NavItem title="Dessert" count="9" />
-                </li>
-                <li>
-                    <NavItem title="Meat and poultry" count="7" />
-                </li> */}
             </ul>
         </nav>
     )
 }
 
 
-// Nav item without destructuring
-// const NavItem = (props) => {
-//     return (
-//         <div className="nav-item">
-//             <p className='nav-item-primary'>{props.title}</p>
-//             <p className='nav-item-secondary'>{props.count}</p>
-//         </div>
-//     )
-// }
-
 // Nav item WITH destructuring
-const NavItem = ({ title, count }) => {
+const NavItem = ({ title, count, onClick }) => {
     return (
-        <div className="nav-item">
+        <div onClick={onClick} className="nav-item">
             <p className='nav-item-primary'>{title}</p>
             <p className='nav-item-secondary'>{count}</p>
         </div>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,11 +1,77 @@
+import { useEffect, useState } from "react";
 import SideNav from "../components/home/SideNav";
 import MainView from "../components/home/MainView";
 
+// URLs
+const FETCH_CATEGORIES = "https://www.themealdb.com/api/json/v1/1/list.php?c=list";
+const FETCH_MEALS = (category) => `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+
+// Effects
+const loadCategories = (setUpFxn) => {
+  fetch(FETCH_CATEGORIES, {})
+  .then((response) => response.json())
+  .then((body) => setUpFxn(body))
+}
+
+const loadFoodItems = (category, setUpFxn) => {
+  fetch(FETCH_MEALS(category), {})
+  .then((response) => response.json())
+  .then((body) => setUpFxn(body))
+}
+
 function Home(){
+
+  const [foodItems, setFoodItems] = useState([]);
+  const setUpFood = (body) => {
+    const meals = body["meals"];
+    const foodItemsList = meals.map((foodItem) => {
+      const item = {
+        id: foodItem["idMeal"],
+        thumbnail: foodItem["strMealThumb"],
+        name: foodItem["strMeal"]
+      };
+      return item;
+    });
+    console.log(`food:`, foodItemsList);
+    setFoodItems(foodItemsList)
+  }
+
+  const [categories, setCategories] = useState([]);
+  const setUpCategories = (body) => {
+    const meals = body["meals"]
+    const catList = meals.map((cat) => {
+      const category = {
+        id: crypto.randomUUID(),
+        title: cat["strCategory"],
+        count: parseInt(Math.random() * 20)
+      }
+      return category
+    });
+    loadFoodItems(catList[0].title, setUpFood)
+    setCategories(catList);
+  }
+
+  
+  const [categorySelected, selectCategory] = useState("");
+  const changeMenu = (category) => {
+    console.log("selected category", category);
+    selectCategory(category)
+  }
+
+  // load all categories once
+  useEffect(() => {
+    loadCategories(setUpCategories)
+  }, [])
+
+  // load food when category changes
+  useEffect(() => {
+    loadFoodItems(categorySelected, setUpFood)
+  }, [categorySelected]);
+
     return(
     <div className='app'>
         <aside className='side-nav'>
-          <SideNav />
+          <SideNav onChangeMenu={changeMenu} categories={categories} />
         </aside>
   
         <main className='main'>
